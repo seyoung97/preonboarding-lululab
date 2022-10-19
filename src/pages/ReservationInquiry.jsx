e417a5f1adc488e30b2b5929ab33500fab1bad16
seyoung97 <108709932+seyoung97@users.noreply.github.com>
2022-10-19 14:55:19 +0900

    Feature:예약 조회 (#3)
    - Design: 예약조회 화면 ui 완성
    - Add: 예약 조회 기능 구현
    - Add: 노쇼 고객 예약 불가 기능 구현
    -  Fix: 예약 조회 조건문 버그 수정

--- src/pages/ReservationInquiry.jsx
+++ src/pages/ReservationInquiry.jsx
@@ -1,8 +1,91 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { mainColor, layout } from '../theme';
 
 const ReservationInquiry = ({ reservatedList, setReservatedList }) => {
+  const [inquiry, setInquiry] = useState('');
+  const [isReservated, setIsReservated] = useState(null);
   console.log(reservatedList);
-  return <div>예약 조회</div>;
+
+  const handleInquiry = () => {
+    for (let i = 0; i < reservatedList.length; i++) {
+      if (reservatedList[i]['user_contact'] === inquiry) {
+        setIsReservated(reservatedList[i]);
+        break;
+      } else {
+        setIsReservated(null);
+      }
+    }
+  };
+
+  // const handleInquiry = () => {
+  //   const result = reservatedList.filter(data => data['user_contact'] === inquiry);
+  //   setIsReservated(result);
+  // };
+
+  console.log(typeof inquiry, inquiry);
+  console.log(isReservated);
+
+  return (
+    <Section>
+      <h1>예약 조회</h1>
+      <h2>예약시 등록한 연락처를 입력해주세요.</h2>
+      <div>
+        연락처
+        <input
+          type='string'
+          onChange={e => {
+            setInquiry(e.target.value);
+          }}
+        ></input>
+      </div>
+      <button onClick={handleInquiry}>조회하기</button>
+      {isReservated !== null ? <div>{isReservated['user_name']}</div> : <div>예약 내역이 없습니다.</div>}
+    </Section>
+  );
 };
+
+const Section = styled.section`
+  width: 450px;
+  margin: auto;
+  margin-top: 100px;
+  h1 {
+    color: ${mainColor};
+    font-size: 25px;
+    font-weight: bold;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+  h2 {
+    padding: 30px 0px;
+    text-align: center;
+    border-radius: 5px;
+    background-color: #eeee;
+    color: #424242;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 30px 10px;
+    font-size: 20px;
+    input {
+      width: 320px;
+      height: 40px;
+      border: 1px solid darkgray;
+      border-radius: 3px;
+    }
+  }
+  button {
+    width: 100%;
+    padding: 10px 0px;
+    margin-top: 15px;
+    border: none;
+    border-radius: 5px;
+    background-color: ${mainColor};
+    color: white;
+    font-size: 20px;
+  }
+`;
 
 export default ReservationInquiry;
